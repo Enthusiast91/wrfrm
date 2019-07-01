45b9504f30a1923331fdd5ac024f8097bac04ab2
Enthusiast91 <Enthusiast91@gmail.com>
2019-07-01 17:32:06 +0400

    Added targets Yandex.metrica. Removed OGRNIP

--- js/common.js
+++ js/common.js
@@ -6,14 +6,14 @@ $(function () {
 	// var instagramSvg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"	width="30px" height="30px" viewBox="0 0 595.3 595.3" enable-background="new 0 0 595.3 595.3" xml:space="preserve">	<path d="M375.2,147.7H218.6c-39.2,0-71.1,31.9-71.1,71.1v158c0,39.2,31.9,71.1,71.1,71.1h156.6		c39.2,0,71.1-31.9,71.1-71.1v-158C446.3,179.6,414.4,147.7,375.2,147.7z M297.2,392.4c-53.1,0-96.3-42.8-96.3-95.3		c0-52.5,43.2-95.3,96.3-95.3s96.3,42.8,96.3,95.3C393.5,349.6,350.3,392.4,297.2,392.4z M395.2,220.6c-12.5,0-22.7-10.1-22.7-22.7		c0-12.5,10.1-22.7,22.7-22.7s22.7,10.1,22.7,22.7C417.9,210.4,407.8,220.6,395.2,220.6z"/>	<path d="M297.6,0C133.3,0,0,133.3,0,297.6s133.3,297.6,297.6,297.6c164.4,0,297.6-133.3,297.6-297.6S462,0,297.6,0z		 M481.4,376.8c0,58.6-47.7,106.2-106.2,106.2H218.6c-58.6,0-106.2-47.7-106.2-106.2v-158c0-58.6,47.7-106.2,106.2-106.2h156.6		c58.6,0,106.2,47.7,106.2,106.2V376.8z"/>	<path d="M297.2,236.9c-33.8,0-61.2,27-61.2,60.2c0,33.2,27.5,60.2,61.2,60.2s61.2-27,61.2-60.2	C358.4,263.9,331,236.9,297.2,236.9z"/></svg>';
 
 	var logoImg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="96.5px" height="171px"	viewBox="0 0 96.5 171" enable-background="new 0 0 96.5 171" xml:space="preserve"> <path d="M23.2,53.4L61,106.3V56.8l33.8-25.7V3.2L23.2,53.4z M62.7,57.6v50.2l13.2-8.2V59l18.9-14.6V33.2L62.7,57.6z	M77.6,100.2l7.7,9.3l9.5-6.2V46.6L77.6,59.8V100.2z M22.9,55.9v94.7l61-40.2l-7.5-9.1l-14.6,9L22.9,55.9z M21.2,153.7v-101L96.5,0 v104.2l-0.4,0.2L21.2,153.7z M3.6,108.7h6.9l4.6-2.7v-5.6L3.6,108.7z M11.5,110v50.6l3.6-2.6v-50.1L11.5,110z M1.7,110.3v57.4 l8.1-5.9v-51.5H1.7z M0,171v-61.8l16.7-12v61.7L0,171z"/></svg>';
-	
+
 	// $('.social-link-vk').html(vkSvg);
 	// $('.social-link-facebook').html(facebookSvg);
 	// $('.social-link-instagram').html(instagramSvg);
 
 	$('.logo-img').html(logoImg);
 	//============ /end svg-icon ============
-	
+
 	//------------------------------------ Обработка событий меню ------------------------------------
 	var pathname = window.location.pathname;
 	var $hamburger = $(".hamburger");
@@ -33,8 +33,8 @@ $(function () {
 				}
 			});
 
-			$hamburger.toggleClass("is-active");			
-			$(".dropdown-menu").stop().slideToggle(); 
+			$hamburger.toggleClass("is-active");
+			$(".dropdown-menu").stop().slideToggle();
 			//Do something else, like open/close menu
 			$("body").toggleClass("no-scroll");
 		});
@@ -51,63 +51,69 @@ $(function () {
 		}
 	});
 
-	
-	//alert(path);
 
-	$(document).ready(function () {
-		//E-mail Ajax Send
-		$("#main-header-application").submit(function () { //Change
-			var th = $(this);
-			$.ajax({
-				type: "POST",
-				url: "php/mail.php", //Change
-				data: th.serialize()
-			}).done(function () {
-				//alert("Thank you!");
-				setTimeout(function () {
-					// Done Functions
-					document.getElementById('main-header-application-submit').style.display = 'none';
-					document.getElementById('main-header-application-thanks').style.display = 'flex';
-					//$('#main-header-application')[0].reset();
-					th.trigger("reset");
-					$("#zayavkaName").attr("placeholder", "").blur();
-					$("#applicationTelephone").attr("placeholder", "").blur();					
-				}, 1000);
-			});
-			return false;
-		});
+	//------------------------------------ Цель ЯМ - нажатие на номер Телефона ------------------------
+	$(".target-ym-phone").click(function () {
+		ym(49476370, 'reachGoal', 'PHONE_PRESSED');
 	});
+
+	
+	//------------------------------------ Главная - Форма захвата ------------------------------------	
+	$("#main-header-application").submit(function () {
+		ym(49476370, 'reachGoal', 'FORM_HOME_PAGE_SUBMIT'); //Цель яндекс метрики
+
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "php/mail.php",
+			data: th.serialize()
+		}).done(function () {
+			//alert("Thank you!");
+			setTimeout(function () {
+				// Done Functions
+				document.getElementById('main-header-application-submit').style.display = 'none';
+				document.getElementById('main-header-application-thanks').style.display = 'flex';
+				//$('#main-header-application')[0].reset();
+				th.trigger("reset");
+				$("#zayavkaName").attr("placeholder", "").blur();
+				$("#applicationTelephone").attr("placeholder", "").blur();
+			}, 1000);
+		});
+		return false;
+	});
+	//$('#button').on('click', function() {
+	//});
 	$('.main-header-button-thanks').click(function () {
 		document.getElementById('main-header-application-thanks').style.display = 'none';
 		document.getElementById('main-header-application-submit').style.display = 'flex'
 	});
 
-	
-	$(document).ready(function () {
-		$("#contacts-application").submit(function () { //Change
-			var th = $(this);
-			$.ajax({
-				type: "POST",
-				url: "php/mail.php", //Change
-				data: th.serialize()
-			}).done(function () {
-				//alert("Thank you!");
-				setTimeout(function () {
-					// Done Functions
-					document.getElementById('contacts-application-ok').style.display = 'none';
-					document.getElementById('contacts-application-thanks').style.display = 'flex';
-					th.trigger("reset");
-					$("#contacts-application-Name").attr("placeholder", "").blur();
-					$("#contacts-application-Email").attr("placeholder", "").blur();					
-					$("#contacts-application-Telephone").attr("placeholder", "").blur();
-				}, 1000);
-			});
-			return false;
+	//------------------------------------ Контакты - Форма захвата ------------------------------------
+	$("#contacts-application").submit(function () {
+		ym(49476370, 'reachGoal', 'FORM_CONTACT_PAGE_SUBMIT'); //Цель яндекс метрики
+ 
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "php/mail.php",
+			data: th.serialize()
+		}).done(function () {
+			//alert("Thank you!");
+			setTimeout(function () {
+				// Done Functions
+				document.getElementById('contacts-application-ok').style.display = 'none';
+				document.getElementById('contacts-application-thanks').style.display = 'flex';
+				th.trigger("reset");
+				$("#contacts-application-Name").attr("placeholder", "").blur();
+				$("#contacts-application-Email").attr("placeholder", "").blur();
+				$("#contacts-application-Telephone").attr("placeholder", "").blur();
+			}, 1000);
 		});
+		return false;
 	});
 	$('.contacts-application-button-thanks').click(function () {
 		document.getElementById('contacts-application-thanks').style.display = 'none';
-		document.getElementById('contacts-application-ok').style.display = 'flex'	
+		document.getElementById('contacts-application-ok').style.display = 'flex'
 	});
 
 	/*
@@ -132,9 +138,9 @@ $(function () {
     console.log(bpy);
     $(".main-header").css("backgroundPosition", "center " + (st*1.1 - 100) + "px");
 	});*/
-	
-	$('.top-scroll').click(function() {
-		$('html, body').stop().animate({scrollTop: 0},  600);
+
+	$('.top-scroll').click(function () {
+		$('html, body').stop().animate({ scrollTop: 0 }, 600);
 	});
 
 	$('a[href^="#"]').on('click', function (event) {
@@ -145,7 +151,7 @@ $(function () {
 			$('html, body').stop().animate({
 				scrollTop: target.offset().top - 70
 			}, 600);
-			
+
 		}
 	});
 
@@ -155,7 +161,7 @@ $(function () {
 	// 		e.preventDefault();
 	// 		location.href = '/portfolio/?scroll_id=portfolio-architecture';
 	// });
-	
+
 	//------------------------------------ Прокручивание при переходе в портфолио --------------------
 	//console.log(window.location);
 	if (window.location.search) {
@@ -169,8 +175,8 @@ $(function () {
 			});
 		}
 	}
-	
-	
+
+
 	// function OnLoad() {
 	// 	var query = window.location.href.split("?")[1]; // результат - строка запроса без адреса страницы "id=someName&userMail=some@mail.com&usText=MemoText"
 	// 	var params = query.split("&");  // результат - массив строк из пар "id=someName", "userMail=some@mail.com", "usText=MemoText"
@@ -191,20 +197,20 @@ $(function () {
 
 
 	//------------------------------------ Эффект Параллакса -----------------------------------------
-	$(window).scroll(function() {  
+	$(window).scroll(function () {
 		var st = $(this).scrollTop();
 		if (st < 800) {
 			var stMain = st * 0.1058;
 			$(".main-header-background").css({	//--- Параллакс на главной странице
-				"transform" : "translate(0%, " + stMain +  "%)"
-			}); 
+				"transform": "translate(0%, " + stMain + "%)"
+			});
 			if (st < 400) {
 				st = st * 0.23;
 				$(".project-header").css({				//--- Параллакс на странице проекта
-					"transform" : "translate(0%, " + st +  "%)"
+					"transform": "translate(0%, " + st + "%)"
 				});
 			}
-		}			
+		}
 	});
 
 	//------------------------------------ Настройка контента на странице проекта --------------------
@@ -215,5 +221,5 @@ $(function () {
 	// 	$(".project-decript-header").removeClass("col-lg-4");	
 	// 	$(".project-decript-header").addClass("col-12");
 	// }
-	
+
 });
